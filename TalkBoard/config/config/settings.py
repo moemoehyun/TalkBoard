92870d57f2278873cffdf6f395c243f97f14e6aa
moemoehyun <gavuryuu@gmail.com>
2024-11-08 01:15:12 +0900

    b1

--- TalkBoard/config/config/settings.py
+++ TalkBoard/config/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['talkboard.onrender.com', "localhost", "127.0.0.1"]
 
