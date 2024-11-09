aaad7c3187c4b0269b0e6cec716bf181871fae7e
moemoehyun <gavuryuu@gmail.com>
2024-11-09 18:26:02 +0900

    c9

--- TalkBoard/config/config/settings.py
+++ TalkBoard/config/config/settings.py
@@ -50,14 +50,14 @@ INSTALLED_APPS = [
 INSTALLED_APPS += ['storages']
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID'), 
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY'),
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME'),
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME'),  # 例: 'us-west-2'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME') # 例: 'us-west-2'
 AWS_QUERYSTRING_AUTH = False  # URLに認証情報を含めない
 AWS_S3_CUSTOM_DOMAIN = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
+    'CacheControl': 'max-age=86400'
 }
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 print(AWS_S3_CUSTOM_DOMAIN)
