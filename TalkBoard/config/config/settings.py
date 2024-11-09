778d20e6179eb73531d5505c87751bc8cd43b877
moemoehyun <gavuryuu@gmail.com>
2024-11-09 22:38:03 +0900

    a6

--- TalkBoard/config/config/settings.py
+++ TalkBoard/config/config/settings.py
@@ -55,7 +55,7 @@ if os.getenv('RENDER'):
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME') # 例: 'us-west-2'
     AWS_QUERYSTRING_AUTH = False  # URLに認証情報を含めない
-    AWS_S3_CUSTOM_DOMAIN = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400'
     }
