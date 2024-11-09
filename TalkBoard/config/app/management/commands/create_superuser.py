6860698f59da44ccf7da7dcd6739448bed399b6c
moemoehyun <gavuryuu@gmail.com>
2024-11-09 19:19:45 +0900

    c11

--- TalkBoard/config/app/management/commands/create_superuser.py
+++ TalkBoard/config/app/management/commands/create_superuser.py
@@ -9,8 +9,8 @@ class Command(BaseCommand):
         User = get_user_model()
         if not User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists():
             User.objects.create_superuser(
-                username=os.environ['DJANGO_SUPERUSER_USERNAME'],
-                email=os.environ['DJANGO_SUPERUSER_EMAIL'],
-                password=os.environ['DJANGO_SUPERUSER_PASSWORD']
+                username=os.environ.get('DJANGO_SUPERUSER_USERNAME'),
+                email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+                password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
             )
         self.stdout.write("Superuser has been created.")
