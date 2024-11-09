fabed2f748abe43e9a71955a0bd71bded6259d45
moemoehyun <gavuryuu@gmail.com>
2024-11-09 13:30:02 +0900

    Render環境でメディアが表示されない不具合の修正

--- TalkBoard/config/config/settings.py
+++ TalkBoard/config/config/settings.py
@@ -42,8 +42,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "app.apps.AppConfig"
+    "app.apps.AppConfig"    
 ]
+
+if os.getenv('RENDER'):
+    INSTALLED_APPS += ['storages']
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID'), 
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME'),
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME'),  # 例: 'us-west-2'
+    AWS_QUERYSTRING_AUTH = False  # URLに認証情報を含めない
+else:
+    #画像ファイルの保存場所を設定
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -201,7 +215,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') #アプリパスワード
-
-#画像ファイルの保存場所を設定
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
