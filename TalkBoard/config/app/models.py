9d55359693c391b1515d7be09339da8461f2639c
moemoehyun <gavuryuu@gmail.com>
2024-11-19 00:27:27 +0900

    a1

--- TalkBoard/config/app/models.py
+++ TalkBoard/config/app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars/', default='avatars/blank_avatar.png')  # デフォルト画像を設定
