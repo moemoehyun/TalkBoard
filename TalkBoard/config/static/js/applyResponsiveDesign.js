7b30e0caa47a3251a149e669913c41a175b07326
moemoehyun <gavuryuu@gmail.com>
2024-11-22 11:23:17 +0900

    a

--- TalkBoard/config/static/js/applyResponsiveDesign.js
+++ TalkBoard/config/static/js/applyResponsiveDesign.js
@@ -1,15 +1,23 @@
-function applyResponsiveDesign() {
-    console.log("現在の画面幅:", window.innerWidth);
-    if (window.innerWidth <= 768) {
-        // モバイルビュー
-        document.body.classList.add("mobile-view");
-        document.body.classList.remove("desktop-view");
-    } else {
-        // デスクトップビュー
-        document.body.classList.add("desktop-view");
-        document.body.classList.remove("mobile-view");
-    }
-}
+// function applyResponsiveDesign() {
+//     console.log("現在の画面幅:", window.innerWidth);
+//     if (window.innerWidth <= 768) {
+//         // モバイルビュー
+//         document.body.classList.add("mobile-view");
+//         document.body.classList.remove("desktop-view");
+//     } else {
+//         // デスクトップビュー
+//         document.body.classList.add("desktop-view");
+//         document.body.classList.remove("mobile-view");
+//     }
 
-applyResponsiveDesign();
-window.addEventListener("resize", applyResponsiveDesign);
+
+//     document.addEventListener('DOMContentLoaded', function () {
+//         const screenType = window.innerWidth <= 768 ? 'mobile' : 'desktop';
+//         document.cookie = `screen_type=${screenType}; path=/`;
+//     });
+// }
+
+// applyResponsiveDesign();
+// window.addEventListener("resize", applyResponsiveDesign);
+
+
