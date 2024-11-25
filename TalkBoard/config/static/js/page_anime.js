2e4ae524e83d561933fbe120e9b259247e511ac5
moemoehyun <gavuryuu@gmail.com>
2024-11-25 16:13:41 +0900

    ページスライドアニメーションのテスト環境構築

---
+++ TalkBoard/config/static/js/page_anime.js
@@ -0,0 +1,27 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const content = document.getElementById("content");
+//     const links = document.querySelectorAll("a"); // 適宜対象を変更
+
+//     links.forEach(link => {
+//         link.addEventListener("click", function (e) {
+//             e.preventDefault(); // 通常の遷移を防ぐ
+//             const targetUrl = this.href;
+
+//             // スライドアウト
+//             content.classList.add("slide-out");
+
+//             // アニメーション後に遷移
+//             setTimeout(() => {
+//                 window.location.href = targetUrl;
+//             }, 500); // アニメーション時間と合わせる
+
+//             // ページがロードされたらスライドインを開始
+//             setTimeout(() => {
+//                 content.classList.add("slide-in");
+//             }, 700); // 遅延を入れるとより自然
+//         });
+//     });
+// });
+
+
+
